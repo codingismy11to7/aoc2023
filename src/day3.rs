13bcc47582ce27cd23d524777cdde3f03874ad9d
Steven Scott <steven.scott@codemettle.com>
2023-12-03 11:52:25 -0500

    day3

--- src/day3.rs
+++ src/day3.rs
@@ -1,89 +1,134 @@
 use crate::util::{get_non_empty_lines, DataLine};
+use regex::Regex;
+use std::collections::HashMap;
+use std::ops::Range;
 
-#[derive(Default, Clone)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 struct PartNum {
     line_num: usize,
-    start_idx: usize,
-    end_idx: usize,
+    indices: Range<usize>,
     num: u64,
 }
 
-struct NumBeingParsed {
-    start_idx: usize,
-    chars: String,
+#[derive(Debug, Eq, PartialEq, Hash)]
+struct Coord {
+    line_num: usize,
+    idx: usize,
+}
+
+fn surrounding_coords(line_num: usize, indices: &Range<usize>) -> Vec<Coord> {
+    let start = indices.start;
+    let end = indices.end;
+
+    let mut ret: Vec<Coord> = vec![];
+
+    let mut add_above_or_below = |line_num: usize| {
+        let start_idx = if start == 0 { 0 } else { start - 1 };
+        (start_idx..end + 1).for_each(|idx| ret.push(Coord { line_num, idx }));
+    };
+
+    if line_num > 1 {
+        add_above_or_below(line_num - 1);
+    }
+    add_above_or_below(line_num + 1);
+
+    if start > 0 {
+        ret.push(Coord {
+            line_num,
+            idx: start - 1,
+        })
+    }
+    ret.push(Coord { line_num, idx: end });
+
+    ret
+}
+
+fn part_nums_from_lines<'a, I>(lines: I) -> Vec<PartNum>
+where
+    I: Iterator<Item = &'a DataLine<'a>>,
+{
+    let num_re = Regex::new(r"\d+").unwrap();
+
+    let part_nums_from_line = |line: &DataLine| {
+        num_re
+            .find_iter(line.line)
+            .map(|m| PartNum {
+                line_num: line.line_number,
+                indices: m.range(),
+                num: m.as_str().parse().unwrap_or_else(|_| panic!("{line}")),
+            })
+            .collect::<Vec<_>>()
+    };
+
+    lines.flat_map(part_nums_from_line).collect()
 }
 
 fn doit(data: &String) -> u64 {
     let lines = get_non_empty_lines(data).collect::<Vec<_>>();
 
-    let mut part_nums = vec![
-        PartNum {
-            ..Default::default()
-        };
-        0
-    ];
-
-    let mut add_part_num = |line: &DataLine, being_parsed: NumBeingParsed, end_idx: usize| {
-        part_nums.push(PartNum {
-            line_num: line.line_number,
-            start_idx: being_parsed.start_idx,
-            end_idx,
-            num: being_parsed
-                .chars
-                .parse()
-                .unwrap_or_else(|_| panic!("{line}")),
-        });
-    };
-
-    lines.iter().for_each(|line| {
-        let was_being_parsed =
-            line.line
-                .char_indices()
-                .fold(None::<NumBeingParsed>, |curr_parsing, (idx, char)| {
-                    if char.is_ascii_digit() {
-                        curr_parsing
-                            .map(|being_parsed| NumBeingParsed {
-                                chars: format!("{}{}", being_parsed.chars, char),
-                                ..being_parsed
-                            })
-                            .or_else(|| {
-                                Some(NumBeingParsed {
-                                    start_idx: idx,
-                                    chars: String::from(char),
-                                })
-                            })
-                    } else {
-                        match curr_parsing {
-                            None => curr_parsing,
-                            Some(being_parsed) => {
-                                add_part_num(line, being_parsed, idx - 1);
-                                None
-                            }
-                        }
-                    }
-                });
-
-        if let Some(being_parsed) = was_being_parsed {
-            add_part_num(line, being_parsed, line.line.len() - 1);
-        }
-    });
+    let part_nums = part_nums_from_lines(lines.iter());
 
     let two_d_board = lines
         .iter()
         .map(|line| line.line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    let char_at = |line_num: usize, idx: usize| {
-        let line = two_d_board.get(line_num - 1)?;
-        let &char = line.get(idx)?;
+    let char_at = |c: &Coord| {
+        let line = two_d_board.get(c.line_num - 1)?;
+        let &char = line.get(c.idx)?;
         Some(char)
     };
 
-    0
+    let adjacent_to_symbol = |part_num: &PartNum| {
+        surrounding_coords(part_num.line_num, &part_num.indices)
+            .iter()
+            .any(|coord| char_at(coord).is_some_and(|c| !c.is_ascii_digit() && (c != '.')))
+    };
+
+    part_nums
+        .iter()
+        .filter(|&pn| adjacent_to_symbol(pn))
+        .fold(0, |acc, pn| acc + pn.num)
 }
 
 fn doit2(data: &String) -> u64 {
-    0
+    let lines = get_non_empty_lines(data).collect::<Vec<_>>();
+
+    let part_nums = part_nums_from_lines(lines.iter());
+
+    let mut coords_to_pns: HashMap<Coord, &PartNum> = HashMap::new();
+    part_nums.iter().for_each(|pn| {
+        pn.indices.clone().for_each(|idx| {
+            coords_to_pns.insert(
+                Coord {
+                    line_num: pn.line_num,
+                    idx,
+                },
+                pn,
+            );
+        })
+    });
+
+    let mut total = 0u64;
+
+    lines.iter().for_each(|line| {
+        line.line.char_indices().for_each(|(idx, char)| {
+            if char == '*' {
+                let touching = surrounding_coords(line.line_number, &(idx..idx + 1));
+                let mut parts: HashMap<&PartNum, bool> = HashMap::new();
+                touching.iter().for_each(|c| {
+                    coords_to_pns.get(c).iter().for_each(|pn| {
+                        parts.insert(pn, true);
+                    });
+                });
+                if parts.len() == 2 {
+                    total += parts.keys().fold(1, |acc, pn| acc * pn.num)
+                }
+            }
+        })
+    });
+
+    total
 }
 
 #[cfg(test)]
@@ -93,26 +138,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn t1() {
-        let data = &read_file_panic("./data/day3/part1/test.txt");
+    fn t() {
+        let data = &read_file_panic("./data/day3/test.txt");
         let answer = doit(data);
-        assert_eq!(answer, 4361)
-    }
+        assert_eq!(answer, 4361);
 
-    #[test]
-    fn t2() {
-        let data = &read_file_panic("./data/day3/part2/test.txt");
         let answer = doit2(data);
-        assert_eq!(answer, 0)
+        assert_eq!(answer, 467835);
     }
 
     #[test]
     fn d() {
         let data = &read_file_panic("./data/day3/data.txt");
         let answer = doit(data);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 535235);
 
         let answer = doit2(data);
-        assert_eq!(answer, 0)
+        assert_eq!(answer, 79844424)
     }
 }
