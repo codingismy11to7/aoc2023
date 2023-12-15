d7eb0f22f8bceb6f9896c65a0365681b59d4c257
Steven Scott <steven.scott@codemettle.com>
2023-12-15 00:24:04 -0500

    backport p2 to rust

---
+++ scala/day13/README.md
@@ -0,0 +1,6 @@
+turns out rust and scala are within a few ms of each
+other. and had to backport from scala because i couldn't
+solve it in rust. if i'd read the instructions better,
+though, it probably would have been fine
+
+obvs could be doing a stupid that is slowing rust way down
--- src/day13.rs
+++ src/day13.rs
@@ -2,14 +2,43 @@ use tailcall::tailcall;
 
 use crate::util::get_lines;
 
-type Block = Vec<Vec<char>>;
+#[derive(Clone)]
+struct BlockInfo {
+    num_rows: usize,
+    num_cols: usize,
+}
+struct Block {
+    data: Vec<Vec<char>>,
+    info: BlockInfo,
+}
 struct NumBlock {
     cols: Vec<u64>,
     rows: Vec<u64>,
 }
 enum MirrorPoint {
-    Column(u64),
-    Row(u64),
+    Column(usize),
+    Row(usize),
+}
+
+impl MirrorPoint {
+    fn value_if(&self, is_col: bool) -> Option<usize> {
+        match self {
+            MirrorPoint::Column(c) => {
+                if is_col {
+                    Some(*c)
+                } else {
+                    None
+                }
+            }
+            MirrorPoint::Row(r) => {
+                if is_col {
+                    None
+                } else {
+                    Some(*r)
+                }
+            }
+        }
+    }
 }
 
 fn convert_to_number<'a>(chars: impl Iterator<Item = &'a char>) -> u64 {
@@ -27,16 +56,17 @@ fn convert_to_number<'a>(chars: impl Iterator<Item = &'a char>) -> u64 {
     rec(0, 1, chars)
 }
 
-fn block_to_numblock(block: Block) -> NumBlock {
+fn block_to_numblock(block: &Block) -> NumBlock {
     fn block_col(block: &Block, col: usize) -> impl Iterator<Item = &char> {
-        (0..block.len()).map(move |i| &block[i][col])
+        (0..block.data.len()).map(move |i| &block.data[i][col])
     }
 
     let rows = block
+        .data
         .iter()
         .map(|line| convert_to_number(line.iter()))
         .collect();
-    let cols = (0..block[0].len())
+    let cols = (0..block.data[0].len())
         .map(|col| block_col(&block, col))
         .map(convert_to_number)
         .collect();
@@ -69,34 +99,73 @@ fn is_mirrorpoint(nums: &Vec<u64>, mirror_after: usize) -> bool {
     rec(nums, mirror_after, 1)
 }
 
-fn find_mirrorpoint(nums: &Vec<u64>) -> Option<usize> {
-    (0..nums.len() - 1).find(|&mirror| is_mirrorpoint(nums, mirror))
+fn block_to_smudge_corrected(block: &Block) -> Vec<Block> {
+    let mut ret = vec![];
+
+    (0..block.info.num_rows).for_each(|row| {
+        (0..block.info.num_cols).for_each(|col| {
+            let mut data = block.data.clone();
+            data[row][col] = if data[row][col] == '.' { '#' } else { '.' };
+            ret.push(Block {
+                data,
+                info: block.info.clone(),
+            })
+        })
+    });
+
+    ret
 }
 
-fn numblock_to_mirrorpoint_opt(block: NumBlock) -> Option<MirrorPoint> {
-    find_mirrorpoint(&block.rows)
-        .map(|i| MirrorPoint::Row(i as u64))
-        .or_else(|| find_mirrorpoint(&block.cols).map(|i| MirrorPoint::Column(i as u64)))
+fn find_mirrorpoint(nums: &Vec<u64>, but_not: Option<usize>) -> Option<usize> {
+    (0..nums.len() - 1)
+        .filter(|i| !but_not.iter().any(|j| j == i))
+        .find(|&mirror| is_mirrorpoint(nums, mirror))
 }
 
-fn numblock_to_mirrorpoint(block: NumBlock) -> MirrorPoint {
-    numblock_to_mirrorpoint_opt(block).unwrap()
+fn numblock_to_mirrorpoint_opt(
+    but_not: &Option<MirrorPoint>,
+    block: NumBlock,
+) -> Option<MirrorPoint> {
+    find_mirrorpoint(
+        &block.rows,
+        but_not.iter().flat_map(|o| o.value_if(false)).next(),
+    )
+    .map(MirrorPoint::Row)
+    .or_else(|| {
+        find_mirrorpoint(
+            &block.cols,
+            but_not.iter().flat_map(|o| o.value_if(true)).next(),
+        )
+        .map(MirrorPoint::Column)
+    })
+}
+
+fn numblock_to_mirrorpoint(but_not: &Option<MirrorPoint>, block: NumBlock) -> MirrorPoint {
+    numblock_to_mirrorpoint_opt(but_not, block).unwrap()
 }
 
 fn mirrorpoint_to_num(mp: MirrorPoint) -> u64 {
     match mp {
-        MirrorPoint::Column(c) => c + 1,
-        MirrorPoint::Row(r) => 100 * (r + 1),
+        MirrorPoint::Column(c) => (c as u64) + 1,
+        MirrorPoint::Row(r) => 100 * ((r as u64) + 1),
     }
 }
 
 fn parse_blocks(data: &str) -> Vec<Block> {
     let mut blocks = vec![];
 
+    fn block_from(v: Vec<Vec<char>>) -> Block {
+        let info = BlockInfo {
+            num_rows: v.len(),
+            num_cols: v[0].len(),
+        };
+        Block { data: v, info }
+    }
+
     let last_opt = get_lines(data).fold(None, |acc, line| {
         if line.line.trim().is_empty() {
             if let Some(prev) = acc {
-                blocks.push(prev);
+                blocks.push(block_from(prev));
             }
             None
         } else {
@@ -112,7 +181,7 @@ fn parse_blocks(data: &str) -> Vec<Block> {
     });
 
     if let Some(last) = last_opt {
-        blocks.push(last)
+        blocks.push(block_from(last))
     }
 
     blocks
@@ -120,20 +189,33 @@ fn parse_blocks(data: &str) -> Vec<Block> {
 
 fn doit(data: &str) -> u64 {
     parse_blocks(data)
-        .into_iter()
+        .iter()
         .map(block_to_numblock)
-        .map(numblock_to_mirrorpoint)
+        .map(|b| numblock_to_mirrorpoint(&None, b))
         .map(mirrorpoint_to_num)
         .sum()
 }
 
 fn doit2(data: &str) -> u64 {
-    0
+    parse_blocks(data)
+        .iter()
+        .map(|block| {
+            let old_mirror_point = &numblock_to_mirrorpoint_opt(&None, block_to_numblock(block));
+
+            block_to_smudge_corrected(block)
+                .iter()
+                .map(block_to_numblock)
+                .filter_map(|b| numblock_to_mirrorpoint_opt(old_mirror_point, b))
+                .next()
+                .unwrap()
+        })
+        .map(mirrorpoint_to_num)
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::util::read_file_panic;
+    use crate::util::{print_dur, read_file_panic};
 
     use super::*;
 
@@ -148,16 +230,16 @@ mod tests {
     fn t2() {
         let data = &read_file_panic("./data/day13/test.txt");
         let answer = doit2(data);
-        assert_eq!(answer, 0);
+        assert_eq!(answer, 400);
     }
 
     #[test]
     fn d() {
         let data = &read_file_panic("./data/day13/data.txt");
-        let answer = doit(data);
+        let answer = print_dur("part1", || doit(data));
         assert_eq!(answer, 29213);
 
-        let answer = doit2(data);
-        assert_eq!(answer, 0);
+        let answer = print_dur("part2", || doit2(data));
+        assert_eq!(answer, 37453);
     }
 }
